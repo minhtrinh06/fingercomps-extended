a5108ab744dac239ce988b89833d3dc0fbd044e4
Eric Liu <latiosworks@gmail.com>
2026-05-21 20:52:46 +1000

    fix: use replacement supabase backend

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Supabase configuration
-const supabaseUrl = 'https://dfbzmtaupuuveeawjsgh.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRmYnptdGF1cHV1dmVlYXdqc2doIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyODMyMTEsImV4cCI6MjA1NTg1OTIxMX0.szNcyWczThjDF1uwFDakDdr30xIgq3pfcCvyXCZg6CY';
+const supabaseUrl = 'https://knyxjptctrqylhxjzggv.supabase.co';
+const supabasePublishableKey = 'sb_publishable_5ZYc4NycMgdD7abXqLK8Bw_ZKP5_9pn';
 
 // Create Supabase client
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabasePublishableKey);
